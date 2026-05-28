35f39b8ff9cd3b2c695de29efbea2a07e3b8b1c4
SweetKaktus <nathan.pintoaguila.gamedesign@gmail.com>
2026-05-28 14:38:12 +0200

    feat: implementing the globalErrorHandler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import { type Express } from "express";
 import mainRouter from "./routes/index.routes";
+import { globalErrorHandler } from "./middlewares/globalErrorHandler";
 
 export const app: Express = express();
 
 app.use(express.json());
 
 app.use("/api", mainRouter);
+
+app.use(globalErrorHandler);
