0ec73d0eeb274052faf1e7f4e7bd054a59bfcc5a
SweetKaktus <nathan.pintoaguila.gamedesign@gmail.com>
2026-06-01 13:29:10 +0200

    fix: add confirm when extract properties from the body request

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -109,7 +109,7 @@ const controller = {
   },
 
   async resetPassword(req: Request, res: Response): Promise<void> {
-    const { currentPassword, newPassword } =
+    const { currentPassword, newPassword, confirm } =
       await resetPasswordBodySchema.parseAsync(req.body);
 
     const connectedUser = await prisma.user.findFirst({
