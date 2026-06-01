810515d7196d8a34b67d7984ee0cacc2db05b72c
Sebvietsky <sebastienfabie@gmail.com>
2026-06-01 16:45:16 +0200

    fix: fixed findAll function on challenges. Now works without page and limit query

--- src/controllers/challenges.controller.ts
+++ src/controllers/challenges.controller.ts
@@ -3,47 +3,49 @@ import { prisma } from "../lib/prisma";
 import { getPaginationParams } from "../utils/pagination.utils";
 import { PaginationOutputSchema, type PaginationParams } from "../schemas/query.schemas";
 
+const selectParams = {
+  id: true,
+  title: true,
+  slug: true,
+  closesAt: true,
+  status: true,
+  createdAt: true,
+  game: {
+    select: {
+      name: true,
+      studio: true,
+      platform: true,
+      coverUrl: true,
+      categories: true,
+    },
+  },
+  challengeCategory: true,
+  difficulty: true,
+  user: {
+    select: {
+      username: true,
+      country: true,
+      profilePicture: true,
+    },
+  },
+  _count: {
+    select: {
+      participations: true,
+      favoritedBy: true,
+      votes: true,
+    },
+  },
+};
+
 const controller = {
-  async findAll(req: Request, res: Response) {
+  async findAll(req: Request, res: Response): Promise<void> {
     const { page, limit }: PaginationParams = await PaginationOutputSchema.parseAsync(req.query);
 
     const { skip, take } = getPaginationParams(page, limit);
 
     const [challenges, total] = await Promise.all([
       prisma.challenge.findMany({
-        select: {
-          id: true,
-          title: true,
-          slug: true,
-          closesAt: true,
-          status: true,
-          createdAt: true,
-          game: {
-            select: {
-              name: true,
-              studio: true,
-              platform: true,
-              coverUrl: true,
-              categories: true,
-            },
-          },
-          challengeCategory: true,
-          difficulty: true,
-          user: {
-            select: {
-              username: true,
-              country: true,
-              profilePicture: true,
-            },
-          },
-          _count: {
-            select: {
-              participations: true,
-              favoritedBy: true,
-              votes: true,
-            },
-          },
-        },
+        select: selectParams,
         skip,
         take,
       }),
@@ -56,6 +58,7 @@ const controller = {
       page,
       limit,
       total,
+      totalPages: Math.ceil(total / limit),
     });
   },
 };
--- src/schemas/query.schemas.ts
+++ src/schemas/query.schemas.ts
@@ -1,8 +1,8 @@
 import z from "zod";
 
 export const PaginationOutputSchema = z.object({
-  page: z.coerce.number<number>().min(1),
-  limit: z.coerce.number<number>().min(1).max(100),
+  page: z.coerce.number<number>().min(1).default(1),
+  limit: z.coerce.number<number>().min(1).max(100).default(20),
 });
 
 export type PaginationParams = z.infer<typeof PaginationOutputSchema>;
