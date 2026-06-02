f5dd30134aa8fc25608e15537e5a122e758d0f02
SweetKaktus <nathan.pintoaguila.gamedesign@gmail.com>
2026-06-02 10:41:14 +0200

    fix: .env.example updated ; challenge routes protected with checkRoles()

--- src/routes/challenge.routes.ts
+++ src/routes/challenge.routes.ts
@@ -1,12 +1,26 @@
 import { Router } from "express";
 import controller from "../controllers/challenges.controller";
+import { checkRoles } from "../middlewares/access-control.middleware";
+import { UserRole } from "../lib/prisma";
 
 const router: Router = Router();
 
 router.get("/", controller.findAll);
 router.get("/:slug", controller.findOne);
-router.post("/", controller.createOne);
-router.patch("/:slug", controller.updateOne);
-router.delete("/:slug", controller.deleteOne);
+router.post(
+  "/",
+  checkRoles([UserRole.admin, UserRole.moderator, UserRole.user]),
+  controller.createOne,
+);
+router.patch(
+  "/:slug",
+  checkRoles([UserRole.admin, UserRole.moderator, UserRole.user]),
+  controller.updateOne,
+);
+router.delete(
+  "/:slug",
+  checkRoles([UserRole.admin, UserRole.moderator, UserRole.user]),
+  controller.deleteOne,
+);
 
 export default router;
