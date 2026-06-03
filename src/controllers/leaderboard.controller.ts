fa5302e3552390c41fa092fcb34adbb63cd1f0a2
Sebvietsky <sebastienfabie@gmail.com>
2026-06-03 15:47:51 +0200

    saving branches

---
+++ src/controllers/leaderboard.controller.ts
@@ -0,0 +1,57 @@
+import { prisma } from "../lib/prisma";
+import type { Request, Response } from "express";
+import { PaginationOutputSchema } from "../schemas/query.schemas";
+import { getPaginationParams } from "../utils/pagination.utils";
+
+const controller = {
+  async mostPlayedChallenges(req: Request, res: Response) {
+    const { page, limit } = await PaginationOutputSchema.parseAsync(req.query);
+
+    const { skip, take } = getPaginationParams(page, limit);
+    const challenges = await prisma.challenge.findMany({
+      orderBy: { participations: { _count: "desc" } },
+      skip,
+      take,
+    });
+
+    res.status(200).json(challenges);
+  },
+
+  async mostActifUsers(req: Request, res: Response) {
+    const { page, limit } = await PaginationOutputSchema.parseAsync(req.query);
+
+    const { skip, take } = getPaginationParams(page, limit);
+
+    await prisma.user.findMany({
+      select: {
+        _count: {
+          select: {
+            participations: true,
+          },
+        },
+      },
+      orderBy: { participations: { _count: "desc" } },
+      skip,
+      take,
+    });
+
+    await prisma.user.findMany({
+      select: {
+        _count: {
+          select: {
+            challenges: true,
+          },
+        },
+      },
+      orderBy: { challenges: { _count: "desc" } },
+      skip,
+      take,
+    });
+
+    // {users, totalParticipation, totalChallengeCreated}
+
+    res.status(200).json();
+  },
+};
+
+export default controller;
